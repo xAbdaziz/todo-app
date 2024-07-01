3ed6bec463ff14cdbd8162df5dde4aaae13217f6
xAbdaziz <me@abdaziz.dev>
2024-07-01 10:40:41 +0300

    backend: Wrap routes with try/catch

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -5,38 +5,54 @@ import tasksModel from '../db/tasksModel';
 const tasksRouter: Router = Router();
 
 tasksRouter.get('/', async (req: Request, res: Response) => {
-  const tasks = await tasksModel.find({})
-  return res.send(tasks)
+  try {
+    const tasks = await tasksModel.find({})
+    return res.send(tasks)
+  } catch {
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
 })
 
 tasksRouter.post('/', async (req: Request, res: Response) => {
-  const { task } = req.body;
-  const taskExists = await tasksModel.exists({ task: task })
-  if (!taskExists) {
-    await tasksModel.create({ task: task })
-    return res.json({ 'message': 'Task Added!' })
+  try {
+    const { task } = req.body;
+    const taskExists = await tasksModel.exists({ task: task })
+    if (!taskExists) {
+      await tasksModel.create({ task: task })
+      return res.json({ 'message': 'Task Added!' })
+    }
+    return res.status(403).json({ 'message': 'Task already exists.' })
+  } catch {
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  return res.status(403).json({ 'message': 'Task already exists.' })
 })
 
 tasksRouter.put('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  const taskExists = await tasksModel.exists({ _id: id })
-  if (!taskExists) {
-    return res.json({ 'message': 'Task doesn\'t exists' })
+  try {
+    const { id } = req.params
+    const taskExists = await tasksModel.exists({ _id: id })
+    if (!taskExists) {
+      return res.json({ 'message': 'Task doesn\'t exists' })
+    }
+    await tasksModel.updateOne({ _id: id }, { isCompleted: req.query.completed })
+    return res.json({ 'message': 'Task Updated!' })
+  } catch {
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  await tasksModel.updateOne({ _id: id }, { isCompleted: req.query.completed })
-  return res.json({ 'message': 'Task Updated!' })
 })
 
 tasksRouter.delete('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  const taskExists = await tasksModel.exists({ _id: id })
-  if (!taskExists) {
-    return res.json({ 'message': 'Task doesn\'t exists' })
+  try {
+    const { id } = req.params
+    const taskExists = await tasksModel.exists({ _id: id })
+    if (!taskExists) {
+      return res.json({ 'message': 'Task doesn\'t exists' })
+    }
+    await tasksModel.deleteOne({ _id: id })
+    return res.json({ 'message': 'Task Deleted!' })
+  } catch {
+    return res.status(500).json({ message: 'Internal Server Error' })
   }
-  await tasksModel.deleteOne({ _id: id })
-  return res.json({ 'message': 'Task Deleted!' })
 })
 
 export default tasksRouter
